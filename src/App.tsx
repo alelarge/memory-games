bf47afa18e4443a0a94c70e5237d3a4f0bf70aa2
armelle09 <alelarge@wecareatwork.com>
2022-11-09 21:14:24 +0100

    Update the app global CSS and refactorize the app to use the store and use CardContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Card from './components/Card/Card';
+import React, { useState } from 'react';
+import { Provider, useSelector } from 'react-redux';
+import { store } from './store';
+import './App.scss';
+import CardContainer from './components/CardContainer/CardContainer';
 
 function App() {
+  const [choise, setChoice] = useState<number>(0)
+  const [Score, setScore] = useState<number>(
+    parseInt(localStorage.getItem('Score') || '0') || Number.MAX_SAFE_INTEGER
+  )
+
   return (
-    <div className="App">
-     <Card content='A' />
-     <Card content='B' />
-     <Card content='C' />
-    </div>
+    <React.StrictMode>
+      <Provider store={store}>
+        <div className="App">
+          <CardContainer />
+        </div>
+      </Provider>
+    </React.StrictMode>
   );
 }
 
